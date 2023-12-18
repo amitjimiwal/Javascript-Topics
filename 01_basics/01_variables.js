b9f877991f4423d6bc9e79c396c0a7588b9ba3cf
Amit Jimiwal <90555965+amitjimiwal@users.noreply.github.com>
2023-12-18 23:18:58 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -13,10 +13,17 @@ accountCity = "Bengaluru"
 
 console.log(accountId);
 
+
+/*
+var accountPassword=10;
+you can redeclare the same variable in var which is not a good practice
+*/
+
 /*
 Prefer not to use var
 because of issue in block scope and functional scope
 */
+
 
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
