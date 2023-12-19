7f416c23472ed26660f98a491ce728c0daa1b6f6
electroo0 <90555965+amitjimiwal@users.noreply.github.com>
2023-12-19 11:04:54 +0530

    conversion add

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,36 +1,41 @@
-let score = "hitesh"
+let score = "hitesh";
 
 //console.log(typeof score);
 //console.log(typeof(score));
 
-let valueInNumber = Number(score)
+//conversion of string to a Number.
+let num = "10";
+let notNum = "Amit";
+let orgNum = Number(notNum); //Nan
+// let orgNum=Number(num); //10
+let valueInNumber = Number(score);
 //console.log(typeof valueInNumber);
 //console.log(valueInNumber);
-
 
 // "33" => 33
 // "33abc" => NaN
 //  true => 1; false => 0
 
-let isLoggedIn = "hitesh"
+let isLoggedIn = "hitesh";
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+let booleanIsLoggedIn = Boolean(isLoggedIn);
 //  console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "hitesh" => true
 
-let someNumber = 33
+let someNumber = 33;
 
-let stringNumber = String(someNumber)
+let stringNumber = String(someNumber);
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
 
+//String(variable) , Boolean(variable) can be used for string and boolean conversion
 // *********************** Operations ***********************
 
-let value = 3
-let negValue = -value
+let value = 3;
+let negValue = -value;
 // console.log(negValue);
 
 // console.log(2+2);
@@ -40,10 +45,10 @@ let negValue = -value
 // console.log(2/3);
 // console.log(2%3);
 
-let str1 = "hello"
-let str2 = " hitesh"
+let str1 = "hello";
+let str2 = " hitesh";
 
-let str3 = str1 + str2
+let str3 = str1 + str2;
 // console.log(str3);
 
 // console.log("1" + 2);
@@ -56,11 +61,11 @@ let str3 = str1 + str2
 // console.log(+true);
 // console.log(+"");
 
-let num1, num2, num3
+let num1, num2, num3;
 
-num1 = num2 = num3 = 2 + 2
+num1 = num2 = num3 = 2 + 2;
 
-let gameCounter = 100
+let gameCounter = 100;
 ++gameCounter;
 console.log(gameCounter);
 
--- 01_basics/09_hoistingletconst.js
+++ 01_basics/09_hoistingletconst.js
@@ -1,12 +1,13 @@
 /*  
-Hoisting - It's a phenomenon in Javascript where you can access the variables and functions even before they are actucally declared in the program
+Hoisting - It's a phenomenon in Javascript where you can access the variables and functions even before they are actually declared in the program
 */
 
 //var variables are hoisted and are declared with undefined keyword
 // but the variables declared with let are const are hoisted but they are not initialized so as such you can't access them before they're declared
 // Refer this https://www.freecodecamp.org/news/javascript-let-and-const-hoisting
 
-console.table([b,z]); //ReferenceError: Cannot access 'b','z' before initialization
+console.log(b);
+console.log(z); //ReferenceError: Cannot access 'b','z' before initialization
 let b=10;
 const z=1;
 
