5ee1a172e743a7ab52ed997e744c970b13388114
electroo0 <90555965+amitjimiwal@users.noreply.github.com>
2023-12-22 12:45:04 +0530

    Add block and shadowing example

---
+++ 01_basics/13_blockandShadowing.js
@@ -0,0 +1,12 @@
+/* 
+ Block = group of statements combined together 
+ Shadowing = When a variable/function shadows the value of the parent scope variable in its scope.
+*/
+
+let a = 10;
+const b = 10;
+{
+  a = 20;
+  console.log(b); // 10
+}
+console.log(a); //10;
