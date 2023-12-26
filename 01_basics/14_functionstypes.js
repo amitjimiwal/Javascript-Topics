fa1f03c7625b912cf9e58a0ef6744510a3be1b0c
electroo0 <90555965+amitjimiwal@users.noreply.github.com>
2023-12-26 11:56:42 +0530

    IIFE function declarations

--- 01_basics/14_functionstypes.js
+++ 01_basics/14_functionstypes.js
@@ -1,13 +1,12 @@
 //FUNCTION STATEMENT or function declaration
-function statement(){
-     console.log("This is a function statement");
+function statement() {
+  console.log("This is a function statement");
 }
 
 //function expression
-let a=function(){
-     console.log("This is a function expression");
-}
-
+let a = function () {
+  console.log("This is a function expression");
+};
 
 //anonymous function
 // function(){
@@ -15,20 +14,33 @@ let a=function(){
 // }
 
 //named function expression
-let asliName=function name(){
-     console.log("Named function Expression");
-}
+let asliName = function name() {
+  console.log("Named function Expression");
+};
 
 //First class functions - ability of functions in javascript to be passed as parameters or returned from Javascript
 
-function outer(param){
-     return function inner(){
-          param();
-     }
+function outer(param) {
+  return function inner() {
+    param();
+  };
 }
-console.log(outer(function(){
-     console.log("Badiya hu bhai");
-})());
+console.log(
+  outer(function () {
+    console.log("Badiya hu bhai");
+  })()
+);
 
+//IIFE Immediately Invoked function expression
 
- 
+(function IIFE() {
+  console.log("I am IIFE");
+})();
+
+function outerIIFE() {
+  console.log("Outer called");
+  (function IIFE() {
+    console.log("I am IIFE");
+  })();
+}
+outerIIFE();
