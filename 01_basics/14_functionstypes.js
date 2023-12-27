5935cd265a548f1eeb24db7605cd54d9980e0057
electroo0 <90555965+amitjimiwal@users.noreply.github.com>
2023-12-27 13:03:28 +0530

    Add first-class functions and callback function examples

--- 01_basics/14_functionstypes.js
+++ 01_basics/14_functionstypes.js
@@ -18,7 +18,9 @@ let asliName = function name() {
   console.log("Named function Expression");
 };
 
-//First class functions - ability of functions in javascript to be passed as parameters or returned from Javascript
+
+//First class functions - ability of functions in javascript to be passed as parameters or returned from Javascript.
+// When functions in a programming language are treated like any other variable then that programming language is known to have first-class functions. In javascript, the functions are known as the first-class citizens, which means functions can do what any other variables can. First-class functions javascript get this ability by treating the functions as an object.
 
 function outer(param) {
   return function inner() {
---
+++ 01_basics/15_callbackfunction.js
@@ -0,0 +1,25 @@
+// Callback function- function passed as an argument to another function
+function sayHello(){
+    return "Hello";
+}
+function greet(greeting,name){
+     console.log(greeting(),name);
+}
+greet(sayHello,'John');
+
+
+//Blocking the main thread - we are waiting for an operation to get completed before we can move on to the next line of code, so our main thread i.e, the call stack is blocked for further execution until the operation is completed.
+//That's why we use asynchronous programming to avoid blocking the main thread that is being handled by the event loop.
+
+setTimeout(() => {
+     console.log("I am an asynchronous function");
+}, 5000);
+const a=10;
+function ClickCount(){
+     let clicked=0;
+     btn.addEventListener('click',function (){
+          clicked++;
+         console.log(`Button clicked ${clicked} times`);
+     });
+}
+ClickCount();;
